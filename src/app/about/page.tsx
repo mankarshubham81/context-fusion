84dda57bd3f5b5cbb886db7247d29c01695978bf
Shubham Mankar <mankarshubham81@gmail.com>
2024-10-12 22:35:30 +0530

    add about route

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const About = () => {
+
+  return (
+    <div className="mt-16">
+        <div className='text-customBlue font-semibold mt-4 p-4' >About Section</div>
+    </div>
+  );
+};
+
+export default About;
