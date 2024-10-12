5db5d1dc2e5244e6a28951c3f44a290ad74be825
Shubham Mankar <mankarshubham81@gmail.com>
2024-10-12 22:21:48 +0530

    commented unnecessary code

--- src/app/blog/layout.tsx
+++ src/app/blog/layout.tsx
@@ -1,7 +1,7 @@
 // app/blog/layout.tsx
 
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer'; // Assuming Footer is also used
+// import Navbar from '@/components/Navbar';
+// import Footer from '@/components/Footer'; // Assuming Footer is also used
 import React from 'react';
 
 export default function BlogLayout({
