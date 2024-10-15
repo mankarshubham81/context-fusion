2a5aad6cd69e403cbb8f9cca7db834a26c30f1cd
Shubham Mankar <mankarshubham81@gmail.com>
2024-10-15 21:13:38 +0530

    new title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>{"Context Fusion"}</title>
+      </head>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
           >
