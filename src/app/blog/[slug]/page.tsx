902fcb1a850c2edd7b5410b12323b047d2ae404e
Shubham Mankar <mankarshubham81@gmail.com>
2024-10-21 11:11:58 +0530

    fixed date format to IST

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -28,6 +28,26 @@ const Blog = async ({ params }: BlogProps) => {
       excerpt
     }`;
 
+  // Helper function to format date for SEO and user readability
+  // Note: name seprate function for it for use this function for multiple cases 
+  const formatToShortIST = (dateString: string) => {
+    const options: Intl.DateTimeFormatOptions = {
+      year: 'numeric',
+      month: 'short',
+      day: '2-digit',
+      timeZone: 'Asia/Kolkata',
+      // year: 'numeric',
+      // month: 'long',
+      // day: 'numeric',
+      // hour: '2-digit',
+      // minute: '2-digit',
+      // second: '2-digit',
+      // timeZone: 'Asia/Kolkata',
+      // timeZoneName: 'short',
+    };
+    return new Intl.DateTimeFormat('en-IN', options).format(new Date(dateString));
+  };
+
   const post: BlogPost = await sanityClient.fetch(query, { slug });
   // console.log("ppp",post)
 
@@ -47,7 +67,7 @@ const Blog = async ({ params }: BlogProps) => {
         <div className="flex items-center mb-6 text-gray-600 dark:text-gray-300">
           <span >By {post.author || 'Unknown Author'}</span>
           <span className="mx-2">|</span>
-          <span >{post.publishedAt}</span>
+          <span >{formatToShortIST(post.publishedAt)}</span>
         </div>
         {post.mainImage?.asset?.url && (
           <div className="mb-6">
--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,7 +1,26 @@
-// components/BlogPost.tsx
 import Link from 'next/link';
 import Image from 'next/image';
 import Head from 'next/head';
+
+// Helper function to format date for SEO and user readability
+// Note: name seprate function for it for use this function for multiple cases 
+const formatToShortIST = (dateString: string) => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+    timeZone: 'Asia/Kolkata',
+    // year: 'numeric',
+    // month: 'long',
+    // day: 'numeric',
+    // hour: '2-digit',
+    // minute: '2-digit',
+    // second: '2-digit',
+    // timeZone: 'Asia/Kolkata',
+    // timeZoneName: 'short',
+  };
+  return new Intl.DateTimeFormat('en-IN', options).format(new Date(dateString));
+};
 
 interface BlogPostProps {
   title: string;
@@ -48,7 +67,7 @@ const BlogPost = ({ slug, title, excerpt, category, date, author, imageUrl }: Bl
                 name: 'Context Fusion',
                 logo: {
                   '@type': 'ImageObject',
-                  url: 'https://context-fusion.vercel.app/logo.jpg', // Add your website logo URL
+                  url: 'https://context-fusion.vercel.app/context_fusoion.png',
                 },
               },
               mainEntityOfPage: {
@@ -62,54 +81,52 @@ const BlogPost = ({ slug, title, excerpt, category, date, author, imageUrl }: Bl
 
       {/* Blog Post Layout */}
       <article className="relative h-full rounded-lg bg-white dark:bg-gray-900 dark:text-gray-100 overflow-hidden shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out">
-            <header className="relative">
-              <Image
-                src={imageUrl}
-                className="w-full h-52 object-cover lg:h-64"
-                loading="lazy"
-                width={800}
-                height={450}
-                alt={title} // Ensure good alt text for accessibility
-              />
-            </header>
-            <div className="p-6">
-              <h1 className="text-2xl font-bold leading-tight mb-2" itemProp="headline">
-                {title}
-              </h1>
-              <div className="flex items-center text-sm text-gray-500 mb-2">
-                <time dateTime={date} itemProp="datePublished">
-                  {date}
-                </time>
-                <span className="mx-2">•</span>
-                <span itemProp="author">{author}</span>
-              </div>
-              <p className="text-sm text-gray-700 dark:text-gray-300 mb-4">
-                {excerpt}
-              </p>
-              <div className="flex justify-between items-center">
-                <span className="text-white bg-purple-900 rounded-full px-2.5 py-2  text-sm font-semibold">
-                  {category}
-                </span>
-                <Link href={`/blog/${slug}`} className="block">
-                  <button
-                    className={`px-4 py-2 sm:my-2 mx-4 
-                    text-white 
-                    border-2
-                    dark:bg-black
-                    bg-customBlue
-                    transition-all
-                    rounded-md
-                    border-customBlue hover:text-white hover:bg-black dark:hover:bg-customBlue
-                    duration-200 ease-in-out
-                    focus:outline-none focus:ring-2 focus:ring-gray-500`}
-                  >
-                    Read More...
-                  </button>
-                </Link>
-                {/* <button className="text-customBlue text-sm font-semibold underline">
-                </button> */}
-              </div>
-            </div>
+        <header className="relative">
+          <Image
+            src={imageUrl}
+            className="w-full h-52 object-cover lg:h-64"
+            loading="lazy"
+            width={800}
+            height={450}
+            alt={title} // Ensure good alt text for accessibility
+          />
+        </header>
+        <div className="p-6">
+          <h1 className="text-2xl font-bold leading-tight mb-2" itemProp="headline">
+            {title}
+          </h1>
+          <div className="flex items-center text-sm text-gray-500 mb-2">
+            <time dateTime={date} itemProp="datePublished">
+              {formatToShortIST(date)} {/* Shorter date format for SEO */}
+            </time>
+            <span className="mx-2">•</span>
+            <span itemProp="author">{author}</span>
+          </div>
+          <p className="text-sm text-gray-700 dark:text-gray-300 mb-4">
+            {excerpt}
+          </p>
+          <div className="flex justify-between items-center">
+            <span className="text-white bg-purple-900 rounded-full px-2.5 py-2 text-sm font-semibold">
+              {category}
+            </span>
+            <Link href={`/blog/${slug}`} className="block">
+              <button
+                className={`px-4 py-2 sm:my-2 mx-4
+                text-white
+                border-2
+                dark:bg-black
+                bg-customBlue
+                transition-all
+                rounded-md
+                border-customBlue hover:text-white hover:bg-black dark:hover:bg-customBlue
+                duration-200 ease-in-out
+                focus:outline-none focus:ring-2 focus:ring-gray-500`}
+              >
+                Read More...
+              </button>
+            </Link>
+          </div>
+        </div>
       </article>
     </>
   );
